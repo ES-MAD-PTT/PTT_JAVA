7821b3a4f0991a1e1527aad38be5c412177f0cf5
Adrian Garcia Alonso <adrian.garcia@eviden.com>
2024-04-05 12:40:57 +0200

    modifications for shipper report daily

--- tpa/tpasystem/src/main/java/com/atos/services/nominations/ShippersNominationsReportsServiceImpl.java
+++ tpa/tpasystem/src/main/java/com/atos/services/nominations/ShippersNominationsReportsServiceImpl.java
@@ -9,16 +9,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.atos.beans.ComboFilterNS;
+import com.atos.beans.nominations.NominationDetailBean;
+import com.atos.beans.nominations.ShippersNominationReportsDailyBean;
 import com.atos.beans.nominations.ShippersNominationsReportsBean;
 import com.atos.filters.nominations.ShippersNominationsReportsFilter;
+import com.atos.mapper.nominations.NominationMapper;
 import com.atos.mapper.nominations.ShippersNominationsReportsMapper;
 
 @Service("shippersNominationsReportsService")
 public class ShippersNominationsReportsServiceImpl implements ShippersNominationsReportsService{
 	
+	private static final long serialVersionUID = -1474869475323574106L;
+
 	@Autowired
 	private ShippersNominationsReportsMapper mapper;
-	private static final long serialVersionUID = -1474869475323574106L;
+
+	@Autowired
+	private NominationMapper nomMapper;
 	
 	public List<ShippersNominationsReportsBean> selectShippersNomReports(ShippersNominationsReportsFilter filter) {
 		return mapper.selectShippersNomReports(filter);
@@ -34,4 +41,27 @@ public class ShippersNominationsReportsServiceImpl implements ShippersNomination
 		}
 		return map;
 	}
+	
+	@Override
+	public Map<String, BigDecimal> selectZonesNomination() {
+		Map<String, BigDecimal> map = new LinkedHashMap<String, BigDecimal>();
+ 		List<ComboFilterNS> list = nomMapper.selectZonesNomination();
+		for (ComboFilterNS combo : list) {
+			if (combo == null) continue;
+			map.put(combo.getValue(), combo.getKey());
+		}
+		return map;
+	}
+
+	@Override
+	public List<ShippersNominationReportsDailyBean> selectShipperNomReportsDailyDetail(ShippersNominationsReportsBean bean) {
+		return mapper.selectShipperNomReportsDailyDetail(bean);
+	}
+
+	@Override
+	public List<ShippersNominationReportsDailyBean> selectShipperNomReportsParkUnpark(ShippersNominationsReportsBean bean) {
+		return mapper.selectShipperNomReportsParkUnpark(bean);
+	}
+
+
 }
